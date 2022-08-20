91d3c11f47beca995b01db13330dc5631ead060f
Bertug <bertugyemen@outlook.com>
2022-08-20 22:24:21 +0300

    Add files via upload

---
+++ Get-LastBootUpTime.ps1
@@ -0,0 +1,4 @@
+﻿#$computerName = Read-Host "Enter host name"
+$computerName = Get-ComputerInfo | Select-Object -ExpandProperty CsDNSHostName 
+
+Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $computerName | Select-Object -Property CSName, LastBootUpTime
