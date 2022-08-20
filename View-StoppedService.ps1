2766c68a14ce0582d1217447e704363825a5f69d
Bertug <bertugyemen@outlook.com>
2022-08-20 21:59:36 +0300

    View-StoppedService
    View and out stopped services

---
+++ View-StoppedService.ps1
@@ -0,0 +1,6 @@
+﻿
+#$computerName = Read-Host "Enter host name"
+$computerName = Get-ComputerInfo | Select-Object -ExpandProperty CsDNSHostName 
+$StoppedService = Get-Service -ComputerName $computerName | Where-Object -Property Status -eq "Stopped"
+Write-Output $StoppedService
+#$StoppedService | Out-File StoppedService.txt
