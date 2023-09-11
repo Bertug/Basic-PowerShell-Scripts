b7f784bfd7cd0fb31e923d2838bb8e16a5521369
Bertug <bertugyemen@outlook.com>
2023-09-11 23:47:11 +0200

    Add files via upload
    JSON parse - create a table and push to excel

---
+++ JsontoExcelTable.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module ImportExcel
+
+
+# Specify the path to your JSON file
+$jsonFilePath = "some.json"
+# Read the JSON data from the file
+$jsonData = Get-Content -Path $jsonFilePath | Out-String | ConvertFrom-Json
+
+# Create a custom table with all properties
+$table = @()
+foreach ($item in $jsonData) {
+    $row = New-Object PSObject
+    foreach ($property in $item.PSObject.Properties) {
+        $row | Add-Member -MemberType NoteProperty -Name $property.Name -Value $property.Value
+    }
+    $table += $row
+}
+
+# Display the resulting table in a graphical window
+$table | Export-Excel -Path "C:\output1.xlsx" -WorksheetName "Data"
