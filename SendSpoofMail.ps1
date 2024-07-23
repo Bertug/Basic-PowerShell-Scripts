3437ec0bb46546c5c6007642dea0cf4690ee6e52
Bertug <bertugyemen@outlook.com>
2024-07-23 14:46:23 +0200

    Add files via upload

---
+++ SendSpoofMail.ps1
@@ -0,0 +1,10 @@
+﻿Connect-ExchangeOnline
+$smtpServer = "m365x27813413.mail.protection.outlook.com"
+$msg = new-object Net.Mail.MailMessage
+$smtp = new-object Net.Mail.SmtpClient($smtpServer,25)
+$msg.From = "sender@contoso.com"
+$msg.To.Add("admin@M365x27813413.onmicrosoft.com")
+$msg.Subject = "Here is the subject"
+$msg.Body = "Here is the message"
+$smtp.Send($msg)
+$msg.Dispose()
